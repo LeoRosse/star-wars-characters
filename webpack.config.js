6d3458eabb7f17d2307264a1c956b0e7dec3b40c
Leonardo Rosseti <rosseti@extendi.it>
2021-05-09 09:33:38 +0200

    Fix remotes entry path

--- webpack.config.js
+++ webpack.config.js
@@ -3,70 +3,74 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const path = require('path');
 
 const deps = require('./package.json').dependencies;
-module.exports = (_, argv) => ({
-  entry: './src/index',
-  output: {
-    publicPath: argv.mode === 'development' ? 'http://localhost:3001/' : 'https://start-wars-characters.vercel.app',
-    path: path.resolve(__dirname, './build'),
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
-    alias: {
-      src: path.resolve(__dirname, 'src'),
+module.exports = (_, argv) => {
+  const pathDomain =
+    argv.mode === 'development' ? 'http://localhost:3001/' : 'https://start-wars-characters.vercel.app';
+  return {
+    entry: './src/index',
+    output: {
+      publicPath: pathDomain,
+      path: path.resolve(__dirname, './build'),
     },
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'src'),
-    port: 3001,
-    stats: 'normal',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: 'javascript/auto',
-        resolve: {
-          fullySpecified: false,
+    resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+      alias: {
+        src: path.resolve(__dirname, 'src'),
+      },
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'src'),
+      port: 3001,
+      stats: 'normal',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: 'javascript/auto',
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
-      },
-    ],
-  },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'characters',
-      filename: 'remoteEntry.js',
-      remotes: {
-        universe: 'universe@http://localhost:3000/remoteEntry.js',
-      },
-      exposes: {
-        './Characters': './src/app',
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'characters',
+        filename: 'remoteEntry.js',
+        remotes: {
+          universe: `universe@${pathDomain}/remoteEntry.js`,
         },
-        'react-dom': {
-          singleton: true,
-          requiredVersion: deps['react-dom'],
+        exposes: {
+          './Characters': './src/app',
         },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    }),
-  ],
-});
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: deps['react-dom'],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+    ],
+  };
+};
